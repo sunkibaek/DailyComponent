38a6ab313df2224646b45d9aba44b48d58720295
Sunki Baek <sunki.baek@gmail.com>
2018-05-21 12:02:30 +0900

    Edit to accept styles for container and textInput

--- src/components/LabelTextInput.tsx
+++ src/components/LabelTextInput.tsx
@@ -1,35 +1,50 @@
 import React, { Component } from "react";
 import {
   Animated,
+  RegisteredStyle,
   StyleSheet,
   TextInput,
-  TextInputProperties,
-  View
+  TextStyle,
+  View,
+  ViewStyle
 } from "react-native";
 import { human } from "react-native-typography";
 
-type IProps = TextInputProperties & {
+interface IProps {
+  containerStyle?: RegisteredStyle<ViewStyle> | ViewStyle;
   labelText: string;
-};
+  textInputStyle?: RegisteredStyle<TextStyle> | TextStyle;
+}
 interface IState {
+  isFocused: boolean;
   labelAnimation: Animated.Value;
   text: string;
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#F2F1F2",
     borderBottomColor: "#727272",
     borderBottomWidth: 1,
-    height: 42,
+    borderTopLeftRadius: 6,
+    borderTopRightRadius: 6,
+    height: 48,
     justifyContent: "flex-end",
-    paddingBottom: 2
+    paddingBottom: 6,
+    paddingLeft: 8,
+    paddingRight: 8
+  },
+  focusedContainer: {
+    borderBottomColor: "purple"
+  },
+  focusedLabel: {
+    color: "purple"
   },
   label: {
-    bottom: 0,
-    left: 0,
+    bottom: 4,
+    left: 8,
     position: "absolute"
-  },
-  textInput: {}
+  }
 });
 
 class LabelTextInput extends Component<IProps, IState> {
@@ -37,6 +52,7 @@ class LabelTextInput extends Component<IProps, IState> {
     super(props);
 
     this.state = {
+      isFocused: false,
       labelAnimation: new Animated.Value(0),
       text: ""
     };
@@ -44,11 +60,18 @@ class LabelTextInput extends Component<IProps, IState> {
 
   public render() {
     return (
-      <View style={styles.container}>
+      <View
+        style={[
+          styles.container,
+          this.state.isFocused && styles.focusedContainer,
+          this.props.containerStyle
+        ]}
+      >
         <Animated.Text
           style={[
             styles.label,
-            human.caption1,
+            human.body,
+            this.state.isFocused && styles.focusedLabel,
             {
               transform: [
                 {
@@ -60,19 +83,19 @@ class LabelTextInput extends Component<IProps, IState> {
                 {
                   translateX: this.state.labelAnimation.interpolate({
                     inputRange: [0, 1],
-                    outputRange: [0, -6]
+                    outputRange: [0, -16]
                   })
                 },
                 {
                   scaleX: this.state.labelAnimation.interpolate({
                     inputRange: [0, 1],
-                    outputRange: [1, 0.8]
+                    outputRange: [1, 0.6]
                   })
                 },
                 {
                   scaleY: this.state.labelAnimation.interpolate({
                     inputRange: [0, 1],
-                    outputRange: [1, 0.8]
+                    outputRange: [1, 0.6]
                   })
                 }
               ]
@@ -83,16 +106,25 @@ class LabelTextInput extends Component<IProps, IState> {
         </Animated.Text>
 
         <TextInput
-          onBlur={this.moveDownLabel}
-          onFocus={this.moveUpLabel}
+          onBlur={this.handleBlur}
+          onFocus={this.handleFocus}
           onChangeText={this.setText}
-          style={[styles.textInput, human.body]}
+          style={[human.body, this.props.textInputStyle]}
           value={this.state.text}
-          {...this.props}
         />
       </View>
     );
   }
+
+  private handleBlur = () => {
+    this.setState(() => ({ isFocused: false }));
+    this.moveDownLabel();
+  };
+
+  private handleFocus = () => {
+    this.setState(() => ({ isFocused: true }));
+    this.moveUpLabel();
+  };
 
   private moveUpLabel = () => {
     Animated.timing(this.state.labelAnimation, {
--- storybook/stories/index.tsx
+++ storybook/stories/index.tsx
@@ -6,9 +6,19 @@ import LabelTextInput from "../../src/components/LabelTextInput";
 import CenterView from "./CenterView";
 
 storiesOf("TextInput")
-  .addDecorator((getStory: any) => <CenterView>{getStory()}</CenterView>)
-  .add("without text", () => (
-    <ScrollView>
-      <LabelTextInput labelText="Username" style={{ width: 300 }} />
-    </ScrollView>
+  .addDecorator((getStory: any) => (
+    <CenterView>
+      <ScrollView>{getStory()}</ScrollView>
+    </CenterView>
+  ))
+  .add("two inputs", () => (
+    <>
+      <LabelTextInput labelText="Username" textInputStyle={{ width: 300 }} />
+
+      <LabelTextInput
+        labelText="Email"
+        containerStyle={{ marginTop: 16 }}
+        textInputStyle={{ width: 300 }}
+      />
+    </>
   ));
